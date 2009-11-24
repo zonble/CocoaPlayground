ea31e6b9469c12c007db32870d83ecb70e927955
zonble <zonble@gmail.com>
2009-11-24 10:40:45 +0800

    Implements searching.

--- ContactPics/ContactPicsAppDelegate.h
+++ ContactPics/ContactPicsAppDelegate.h
@@ -17,16 +17,21 @@
 	IBOutlet NSArrayController *arrayController;
 	IBOutlet NSScrollView *imageBrowserScrollView;
 	IBOutlet NSView *imageFlowView;
+	IBOutlet NSScrollView *tableScrollView;
 	IBOutlet NSView *containerView;
 	IBOutlet NSView *fullscreenBackgroundView;
 	IBOutlet NSView *fullscreenContainerView;
+	IBOutlet NSSegmentedControl *segmentedControl;
 	
 	id fullscreenView;
 }
 
 - (IBAction)fullscreen:(id)sender;
 - (IBAction)exitFullscreen:(id)sender;
+- (IBAction)search:(id)sender;
 - (IBAction)toogleActiveView:(id)sender;
+- (void)presentContentWithArray:(NSArray *)array;
+- (void)presentDefaultContent;
 - (void)setActiveView:(NSView *)newContentView;
 
 @property (assign) IBOutlet NSWindow *window;
--- ContactPics/ContactPicsAppDelegate.m
+++ ContactPics/ContactPicsAppDelegate.m
@@ -25,12 +25,96 @@
 
 - (void)awakeFromNib
 {
+	[arrayController addObserver:self forKeyPath:@"sortDescriptors" options:NSKeyValueObservingOptionNew context:NULL];
+	[arrayController addObserver:self forKeyPath:@"arrangedObjects" options:NSKeyValueObservingOptionNew context:NULL];
+	[arrayController addObserver:self forKeyPath:@"selectionIndex" options:NSKeyValueObservingOptionNew context:NULL];
 	[(IKImageBrowserView *)imageFlowView setDataSource:self];
 	[(IKImageBrowserView *)imageFlowView setDelegate:self];
 	[self setActiveView:imageBrowserScrollView];
 	
+	[self presentDefaultContent];
+}
+
+- (IBAction)fullscreen:(id)sender
+{
+	if (![fullscreenBackgroundView isInFullScreenMode]) {
+		NSView *view = [[containerView subviews] objectAtIndex:0];
+		[view setFrame:[fullscreenContainerView bounds]];
+		[fullscreenContainerView addSubview:view];
+		[fullscreenBackgroundView enterFullScreenMode:[NSScreen mainScreen] withOptions:[NSDictionary dictionaryWithObjectsAndKeys:[NSNumber numberWithBool:YES], NSFullScreenModeAllScreens, [NSNumber numberWithInt:CGShieldingWindowLevel()], NSFullScreenModeWindowLevel, nil]];
+		[window makeFirstResponder:view];
+		fullscreenView = view;
+	}
+	else {
+		[self exitFullscreen:sender];
+	}
+}
+
+- (IBAction)search:(id)sender
+{
+	NSString *keyword = [[sender stringValue] stringByTrimmingCharactersInSet:[NSCharacterSet whitespaceAndNewlineCharacterSet]];
+	
+	if (![keyword length]) {
+		[self presentDefaultContent];
+		return;
+	}
+	
+	ABSearchElement *search = [ABPerson searchElementForProperty:kABEmailProperty label:nil key:nil value:keyword comparison:kABPrefixMatchCaseInsensitive];
+	NSArray *a = [[ABAddressBook sharedAddressBook] recordsMatchingSearchElement:search];
+	[self presentContentWithArray:a];
+}
+
+- (IBAction)exitFullscreen:(id)sender
+{
+	[fullscreenBackgroundView exitFullScreenModeWithOptions:nil];
+	if (fullscreenView) {
+		[self setActiveView:fullscreenView];
+		[window makeFirstResponder:fullscreenView];
+		fullscreenView = nil;
+	}
+}
+
+- (IBAction)toogleActiveView:(id)sender
+{
+	NSUInteger tag = 0;
+	if ([sender isKindOfClass:[NSMenuItem class]]) {
+		tag = [sender tag];
+	}
+	else if ([sender isKindOfClass:[NSSegmentedControl class]]) {
+		tag = [sender selectedSegment];
+	}
+	
+	if (tag == 0) {
+		[self setActiveView:imageBrowserScrollView];
+	}
+	else if (tag == 1) {
+		[self setActiveView:imageFlowView];
+	}
+	else if (tag == 2) {
+		[self setActiveView:tableScrollView];
+	}
+	[segmentedControl setSelectedSegment:tag];
+}
+
+- (void)setActiveView:(NSView *)newContentView
+{
+	NSRect bounds = [containerView bounds];
+	[newContentView setFrame:bounds];
+	if ([[containerView subviews] count]) {
+		[[[containerView subviews] objectAtIndex:0] removeFromSuperview];
+	}
+	[containerView addSubview:newContentView];
+	[window makeFirstResponder:newContentView];
+}
+
+- (void)presentDefaultContent
+{
 	NSArray *people = [[ABAddressBook sharedAddressBook] people];
-	NSMutableArray *newPeople = [NSMutableArray arrayWithArray:people];
+	[self presentContentWithArray:people];
+}
+- (void)presentContentWithArray:(NSArray *)array
+{	
+	NSMutableArray *newPeople = [NSMutableArray arrayWithArray:array];
 	[newPeople sortUsingComparator:^(id a, id b) {
 		NSString *lastNameA = [(ABRecord *)a valueForProperty:kABLastNameProperty];
 		NSString *lastNameB = [(ABRecord *)b  valueForProperty:kABLastNameProperty];
@@ -41,7 +125,7 @@
 		NSString *firstNameA = [(ABRecord *)a valueForProperty:kABFirstNameProperty];
 		NSString *firstNameB = [(ABRecord *)b  valueForProperty:kABFirstNameProperty];
 		return [firstNameA localizedStandardCompare:firstNameB];		
-	 }];
+	}];
 	NSMutableArray *a = [NSMutableArray array];
 	for (ABRecord *record in newPeople) {
 		if ([record isKindOfClass:[ABPerson class]]) {
@@ -74,51 +158,14 @@
 		}
 	}
 	[arrayController setContent:a];
+}
+
+- (void)observeValueForKeyPath:(NSString *)keyPath ofObject:(id)object change:(NSDictionary *)change context:(void *)context
+{
+	if ([keyPath isEqualToString:@"selectionIndex"]) {
+		[imageFlowView setSelectedIndex:[arrayController selectionIndex]];
+	}
 	[(IKImageBrowserView *)imageFlowView reloadData];
-}
-
-- (IBAction)fullscreen:(id)sender
-{
-	if (![fullscreenBackgroundView isInFullScreenMode]) {
-		NSView *view = [[containerView subviews] objectAtIndex:0];
-		[view setFrame:[fullscreenContainerView bounds]];
-		[fullscreenContainerView addSubview:view];
-		[fullscreenBackgroundView enterFullScreenMode:[NSScreen mainScreen] withOptions:[NSDictionary dictionaryWithObjectsAndKeys:[NSNumber numberWithBool:YES], NSFullScreenModeAllScreens, [NSNumber numberWithInt:CGShieldingWindowLevel()], NSFullScreenModeWindowLevel, nil]];
-		fullscreenView = view;
-	}
-	else {
-		[self exitFullscreen:sender];
-	}
-}
-
-- (IBAction)exitFullscreen:(id)sender
-{
-	[fullscreenBackgroundView exitFullScreenModeWithOptions:nil];
-	if (fullscreenView) {
-		[self setActiveView:fullscreenView];
-		fullscreenView = nil;
-	}
-}
-
-- (IBAction)toogleActiveView:(id)sender
-{
-	if ([sender selectedSegment] == 0) {
-		[self setActiveView:imageBrowserScrollView];
-	}
-	else if ([sender selectedSegment] == 1) {
-		[self setActiveView:imageFlowView];
-	}
-
-}
-
-- (void)setActiveView:(NSView *)newContentView
-{
-	NSRect bounds = [containerView bounds];
-	[newContentView setFrame:bounds];
-	if ([[containerView subviews] count]) {
-		[[[containerView subviews] objectAtIndex:0] removeFromSuperview];
-	}
-	[containerView addSubview:newContentView];
 }
 
 #pragma mark -
@@ -133,6 +180,19 @@
 	[NSApp terminate:self];
 }
 
+- (BOOL)validateMenuItem:(NSMenuItem *)menuItem
+{
+	if ([menuItem action] == @selector(toogleActiveView:)) {
+		if ([menuItem tag] == [segmentedControl selectedSegment]) {
+			[menuItem setState:NSOnState];
+		}
+		else {
+			[menuItem setState:NSOffState];
+		}
+	}
+	return YES;
+}
+
 #pragma mark -
 
 - (NSUInteger)numberOfItemsInImageFlow:(id)aFlowLayer
@@ -145,6 +205,7 @@
 }
 - (void)imageFlow:(id)aFlowLayer didSelectItemAtIndex:(NSInteger)index
 {
+	[arrayController setSelectionIndex:index];
 }
 - (void)imageFlow:(id)aFlowLayer cellWasDoubleClickedAtIndex:(NSInteger)index
 {
