7f2fbcbfe067c23e8835cd2a4d6f962d0b41faa2
zonble <zonble@gmail.com>
2009-12-05 03:14:24 +0800

    Starts playing the Plurk API.

---
+++ ObjectivePlurk/Classes/ObjectivePlurkAppDelegate.h
@@ -0,0 +1,29 @@
+//
+//  ObjectivePlurkAppDelegate.h
+//  ObjectivePlurk
+//
+//  Created by zonble on 12/5/09.
+//  Copyright Lithoglyph Inc. 2009. All rights reserved.
+//
+
+@interface ObjectivePlurkAppDelegate : NSObject <UIApplicationDelegate> {
+    
+    NSManagedObjectModel *managedObjectModel;
+    NSManagedObjectContext *managedObjectContext;	    
+    NSPersistentStoreCoordinator *persistentStoreCoordinator;
+
+    UIWindow *window;
+    UINavigationController *navigationController;
+}
+
+@property (nonatomic, retain, readonly) NSManagedObjectModel *managedObjectModel;
+@property (nonatomic, retain, readonly) NSManagedObjectContext *managedObjectContext;
+@property (nonatomic, retain, readonly) NSPersistentStoreCoordinator *persistentStoreCoordinator;
+
+@property (nonatomic, retain) IBOutlet UIWindow *window;
+@property (nonatomic, retain) IBOutlet UINavigationController *navigationController;
+
+- (NSString *)applicationDocumentsDirectory;
+
+@end
+
---
+++ ObjectivePlurk/Classes/RootViewController.h
@@ -0,0 +1,17 @@
+//
+//  RootViewController.h
+//  ObjectivePlurk
+//
+//  Created by zonble on 12/5/09.
+//  Copyright Lithoglyph Inc. 2009. All rights reserved.
+//
+
+@interface RootViewController : UITableViewController <NSFetchedResultsControllerDelegate> {
+	NSFetchedResultsController *fetchedResultsController;
+	NSManagedObjectContext *managedObjectContext;
+}
+
+@property (nonatomic, retain) NSFetchedResultsController *fetchedResultsController;
+@property (nonatomic, retain) NSManagedObjectContext *managedObjectContext;
+
+@end
